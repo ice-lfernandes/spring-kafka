eaf0307aabd23cce6c82cf1608eac901aac0cd68
ice_lfernandes <ice_lfernandes@uolinc.com>
2024-02-04 02:04:43 -0300

    adição de propiedades do kafka com explicações

--- Simple-Kafka-Producer-Consumer/pix-consumer/src/main/java/br/com/ldf/pix/consumer/config/ConsumerKafkaConfig.java
+++ Simple-Kafka-Producer-Consumer/pix-consumer/src/main/java/br/com/ldf/pix/consumer/config/ConsumerKafkaConfig.java
@@ -28,6 +28,20 @@ public class ConsumerKafkaConfig {
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        // configuração do número máximo de registros que o consumidor pode buscar por vez que bater no cluster
+        // cenário aonde depende da latência de rede para se comunicar ao cluster
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+        // configuração para que o consumidor busque os registros mais recentes a partir de quando ele foi iniciado
+        // pode ser utilizado também o "earliest" para buscar desde o início do tópico
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        // configuração para que o consumidor não crie tópicos automaticamente
+        // por default ele é true
+        props.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG, true);
+        // configuração para que o consumidor não comite automaticamente
+        // por default ele é true e ele só da o ACK quando finaliza o método que está consumindo o evento
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
